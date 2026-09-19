e10534e5fd657b511560e5d0cc4608967f62c589
vini <vinidemoraes05@gmail.com>
2026-09-19 09:37:20 -0300

    chore: adds the fixed header with scroll

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import Banner from "../components/layout/Banner";
 import Categories from "../components/layout/Categories";
 import Featured from "../components/layout/Featured";
@@ -7,17 +8,43 @@ import Newsletter from "../components/layout/Newsletter";
 import TextSlide from "../components/layout/TextSlide";
 
 export default function Home() {
+  const [showFixedHeader, setShowFixedHeader] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setShowFixedHeader(window.scrollY >= 200);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
 
   return (
     <div className="main-content">
+      <div
+        className={`
+          fixed top-0 left-0 z-50 w-full p-4
+          transition-all duration-300
+          ${
+            showFixedHeader
+              ? "translate-y-0 opacity-100"
+              : "-translate-y-full opacity-0 pointer-events-none"
+          }
+        `}
+      >
+        <Header />
+      </div>
+
       <Header />
       <Hero />
-      <TextSlide/>
+      <TextSlide />
       <Featured />
       <Banner />
       <Categories />
       <Newsletter />
     </div>
-  )
-
+  );
 }
