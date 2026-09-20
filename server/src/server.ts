937ede77d7f63dde2e2d3a81b12fa9b4e87e5307
vini <vinidemoraes05@gmail.com>
2026-09-19 21:03:58 -0300

    chore: add session via pg

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,39 @@
 import express, { type Express, type Request, type Response } from 'express';
 import * as path from "node:path";
+import session from "express-session";
+import pgSession from "connect-pg-simple";
+import dotenv from "dotenv";
+import { pool } from "./lib/db.js";
 
+// load envs 
+dotenv.config();
+
+const PgSession = pgSession(session);
+const _dirname = process.cwd();
 const app: Express = express();
 
+// config session store via pg-node
+app.use(
+  session({
+    store: new PgSession({
+      pool,
+    }),
+
+    secret: process.env.SESSION_SECRET!,
+
+    resave: false,
+    saveUninitialized: false,
+
+    cookie: {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "lax",
+      maxAge: 1000 * 60 * 60 * 24 * 7,
+    },
+  })
+);
+
 // serving static file from public folder
-const _dirname = process.cwd();
 app.use(express.static(path.join(_dirname, "public")));
 
 // default to listen the server
